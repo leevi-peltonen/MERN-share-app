5aa14e2d41b50554370680c160cc7142ce5eb032
Leevi Peltonen <leevi.peltonen@icloud.com>
2022-02-17 08:59:36 +0200

    mobile view sidebar, header

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -3,15 +3,19 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" exact element={<Users />} />
-        <Route path="/places/new" exact element={<NewPlace />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" exact element={<Users />} />
+          <Route path="/places/new" exact element={<NewPlace />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 };
